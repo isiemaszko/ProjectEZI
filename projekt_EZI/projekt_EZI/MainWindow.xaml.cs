e4e53d09d691163c2679f2a879bff4370985e902
isiemaszko <izabela.siemaszko@onet.pl>
2021-12-30 14:40:32 +0100

    sorting the ranking by column "ranking"

--- projekt_EZI/projekt_EZI/MainWindow.xaml.cs
+++ projekt_EZI/projekt_EZI/MainWindow.xaml.cs
@@ -24,11 +24,17 @@ namespace projekt_EZI
     public partial class MainWindow : Window
     {
         IncidentList incidentList;
+        Matrix.Matrix Matrix;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void CreateIncidentList()
+        {
+            incidentList = new IncidentList(txtEditor.Text);
+            incidentList.createAdjacencyMatrix();
+        }
         private void openFileBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -36,8 +42,12 @@ namespace projekt_EZI
             if (openFileDialog.ShowDialog() == true)
                 txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
 
-            incidentList = new IncidentList(txtEditor.Text);
-            incidentList.createAdjacencyMatrix();
+            CreateIncidentList();
+        }
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateIncidentList();
         }
 
         private void matrixBtn_Click(object sender, RoutedEventArgs e)
@@ -47,9 +57,9 @@ namespace projekt_EZI
 
         private void createMatrix()
         {
-            var matrix = new Matrix.Matrix(incidentList.matrix);
+            Matrix = new Matrix.Matrix(incidentList.matrix);
+            var dialog = new Window() { Content = Matrix.createMatrix()};
             
-            var dialog = new Window() { Content = matrix.createMatrix() };
             dialog.ShowDialog();
             
         }
@@ -73,12 +83,72 @@ namespace projekt_EZI
 
         }
 
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         void rankingColumnHeader_Click(object sender,
                                            RoutedEventArgs e)
         {
-            int ddd = 0;
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+
+                    Sort(sortBy, direction);
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowUp"] as DataTemplate;
+                    }
+                    else
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowDown"] as DataTemplate;
+                    }
+
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.HeaderTemplate = null;
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+            }
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(RankingListView.Items);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
         }
 
-       
+      
     }
 }
